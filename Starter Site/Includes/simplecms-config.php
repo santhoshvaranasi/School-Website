617e732e638556a29aa636d3ccaf797086ea4d27
santhoshvaranasi <nagasanthosh.varanasi@gmail.com>
2015-06-18 19:01:08 -0400

    crap

--- Starter Site/Includes/simplecms-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('DB_NAME', 'ForumDatabase');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', 'root');
-    define('DB_HOST', 'localhost');
-
-    define('DEFAULT_ADMIN_USERNAME', 'admin');
-    define('DEFAULT_ADMIN_PASSWORD', 'admin');
-?>
